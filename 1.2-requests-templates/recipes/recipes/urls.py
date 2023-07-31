b8f18aeffe0416bdfd263566176967f6ac386b18
Satanas-Gigas <robot-tyaga@yandex.ru>
2023-07-31 23:57:01 +0500

    New Commit

--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -4,8 +4,4 @@ from calculator.views import index_view
 
 urlpatterns = [
     path("<str:dish>/", index_view, name="recipe")
-<<<<<<< HEAD
 ]
-=======
-]
->>>>>>> bd670410bf798fea37ad23fce6cfcbada6cf2fbc
